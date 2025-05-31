40f2b0dd8f32a9a26eeddc7b46dd104a1f831ed0
Joao Victor Angelo <146788838+JoaoVictorAngelo@users.noreply.github.com>
2025-05-31 14:14:02 -0300

    exercicio finalizado, tests funcionais

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,34 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from '@testing-library/react'
+import Post from '.'
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+describe('Teste para o componente Post', () => {
+    test('Deve renderizar o componente corretamente', () => {
+        render(<Post />)
+        expect(screen.getByTestId('comment-button')).toBeInTheDocument()
+        expect(screen.getByTestId('comment-textarea')).toBeInTheDocument()
+    })
+
+    test('Deve permitir a inserção de dois comentários', () => {
+        render(<Post />)
+
+        // Seleciona os elementos pelo data-testid
+        const textarea = screen.getByTestId('comment-textarea')
+        const button = screen.getByTestId('comment-button')
+
+        // Insere o primeiro comentário
+        const primeiroComentario = 'Este é o primeiro comentário'
+        fireEvent.change(textarea, { target: { value: primeiroComentario } })
+        fireEvent.click(button)
+
+        // Insere o segundo comentário
+        const segundoComentario = 'Este é o segundo comentário';
+        fireEvent.change(textarea, { target: { value: segundoComentario } });
+        fireEvent.click(button);
+
+        // Verifica se ambos os comentários estão na lista
+        expect(screen.getAllByRole("listitem")).toHaveLength(2);
+        expect(screen.getByText(primeiroComentario)).toBeInTheDocument();
+        expect(screen.getByText(segundoComentario)).toBeInTheDocument();
     });
 });
+
